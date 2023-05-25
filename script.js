6c3f5fa1aead4669e442d27419b31f1cacaded85
ModaPro1 <87425297+ModaPro1@users.noreply.github.com>
2023-05-25 22:58:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  // Navbar Toggler
+  $(".toggler-nav").on("click", () => {
+    $(".navbar .items").toggleClass("show")
+  })
+  // Navbar Sticky
+  $(window).on("scroll", () => {
+    let scrollTop = $(window).scrollTop()
+    if(scrollTop >= 200 && scrollTop <= 500) {
+      $(".landing .navbar").addClass("scrolling")
+      $(".landing .navbar").removeClass("stick")
+      $(".landing .navbar .items").removeClass("show")
+    }else if(scrollTop >= 500) {
+      $(".landing .navbar").removeClass("scrolling")
+      $(".landing .navbar").addClass("stick")
+    }else {
+      $(".landing .navbar").removeClass("scrolling")
+      $(".landing .navbar .items").removeClass("show")
+    }
+
+    // Add Scrolling class when scroll untill reaching 500 then remove it and add stick
+    // For the animations
+    if(scrollTop > $(".services").offset().top - 600) {
+      $(".services").addClass("animate__animated animate__backInUp")
+    }
+    if(scrollTop > $(".menu").offset().top - 600) {
+      $(".menu .boxes .box").addClass("animate__animated animate__fadeInUp")
+    }
+    if(scrollTop > $(".reservation").offset().top - 600) {
+      $(".reservation form").addClass("animate__animated animate__fadeInUp")
+    }
+    if(scrollTop > $(".testimonials").offset().top - 600) {
+      $(".testimonials").addClass("animate__animated animate__bounceIn")
+    }
+  })
+})
